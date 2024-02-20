7ed53140512718d514d3e6c015e89f074f148b4c
Ciurea Cristian <123569444+Ciureacristian@users.noreply.github.com>
2024-02-20 07:39:55 -1000

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from "react"
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
+    axios
+      .get('http://localhost:8080/profile')
+      .then((response) => {
+        const { data } = response;
 
+        setName(data.name);
+      })
+      .catch((response) => {
+        const { code } = response;
+
+        if (code === 'ERR_NETWORK') {
+          setMessage('Could not fetch data');
+        }
+      });
   }, []);
-
   return (
-    <>
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      {name.trim().length <= 0 ? '...loading' : name}
     </h1>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
